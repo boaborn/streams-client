ec485905794d264c1a134324cc8426234ba01e2b
Michael <michaelzhaojq@hotmail.com>
2020-01-22 14:22:42 +1300

    add api actions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import reduxThunk from 'redux-thunk'
 
 import App from './components/App'
 import reducers from './reducers'
@@ -10,7 +11,7 @@ import reducers from './reducers'
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(
   reducers,
-  composeEnhancers(applyMiddleware())
+  composeEnhancers(applyMiddleware(reduxThunk))
 )
 
 ReactDOM.render(
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form' // rename reducer as formReducer
 import authReducer from './authReducer'
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer // connect redux form to my reducer
 })
