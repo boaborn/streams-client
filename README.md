c8d0031894d15c7c8f782a2fcc4d0cacaf252784
Michael <michaelzhaojq@hotmail.com>
2020-01-23 15:39:47 +1300

    add own history object

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 #README
+
+History object
+(dispatch, getState)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form' // rename reducer as formReducer
 import authReducer from './authReducer'
+import streamReducer from './streamReducer'
 
 export default combineReducers({
   auth: authReducer,
-  form: formReducer // connect redux form to my reducer
+  form: formReducer, // connect redux form to my reducer
+  streams: streamReducer
 })
